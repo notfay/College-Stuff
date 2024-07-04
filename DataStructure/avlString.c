2df28471dc2f89548f90624afb4b9d482ee36e01
Teuku Fayaz Ahsan <143577621+notfay@users.noreply.github.com>
2024-07-04 11:30:01 +0700

    update

--- DataStructure/avlString.c
+++ DataStructure/avlString.c
@@ -5,11 +5,12 @@
 #include <stdlib.h>
 
 typedef struct node {
-    char isbn[20];
+
     struct node* left;
     struct node* right;
     int height;
-} node;
+    char isbn[20];
+}node;
 
 node* create(char isbn[]) {
     node* head = malloc(sizeof(node));
@@ -17,7 +18,6 @@ node* create(char isbn[]) {
     head->right = NULL;
     strcpy(head->isbn, isbn);
     head->height = 1;
-
     return head;
 }
 
@@ -39,11 +39,11 @@ int getBal(node* n) {
     return height(n->left) - height(n->right);
 }
 
-node* leftR(node* t) {
-    node* s = t->right;
-    node* x2 = s->left;
+node* rightR(node* t) {
+    node* s = t->left;
+    node* x2 = s->right;
 
-    s->left = t;
+    s->right = t;
     t->right = x2;
 
     t->height = max(height(t->left), height(t->right)) + 1;
@@ -52,20 +52,18 @@ node* leftR(node* t) {
     return s;
 }
 
+node* leftR(node* t) {
+    node* s = t->right;
+    node* x2 = s->left;
 
-node* rightR(node* t) {
-    node* s = t->left;
-    node* x2 = s->right;
+    s->left = t;
+    t->right = x2;
 
-    s->right = t;
-    t->left = x2;
-
-    t->height = max(height(t->left), height(t->right)) + 1;
+     t->height = max(height(t->left), height(t->right)) + 1;
     s->height = max(height(s->left), height(s->right)) + 1;
 
     return s;
 }
-
 
 node* insert(node* root, char isbn[]) {
     if(root == NULL) {
@@ -75,7 +73,7 @@ node* insert(node* root, char isbn[]) {
     if(strcmp(root->isbn, isbn) < 0) {
         root->left = insert(root->left, isbn);
     }
-    else if(strcmp(root->isbn, isbn) > 0) {
+    else if (strcmp(root->isbn, isbn) > 0) {
         root->right = insert(root->right, isbn);
     }
     else {
@@ -91,15 +89,23 @@ node* insert(node* root, char isbn[]) {
     if(bal < -1 && strcmp(root->right->isbn, isbn) > 0) {
         return leftR(root);
     }
-    if(bal > 1 && strcmp(root->left->isbn, isbn) > 0) {
+     if(bal > 1 && strcmp(root->left->isbn, isbn) > 0) {
         root->left = leftR(root->left);
         return rightR(root);
     }
-    if (bal < -1 && strcmp(root->right->isbn, isbn) < 0) {
-        root->right = rightR(root->right);
+    if(bal < -1 && strcmp(root->left->isbn, isbn) < 0) {
+        root->right = leftR(root->right);
         return leftR(root);
     }
 
     return root;
 
+
 }
+
+
+int main () {
+
+    return 0;
+}
+
